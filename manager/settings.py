ec9ac578b0eea99194b61b5b7b31555965aeccc5
guile.hm <guile.hm@hotmail.com>
2018-03-29 17:22:47 -0300

    Class Company in model

--- manager/settings.py
+++ manager/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     # my apps
     'core',
     'register',
+
+    # third apps
+    'multiselectfield',
 ]
 
 MIDDLEWARE = [
--- register/models.py
+++ register/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from multiselectfield import MultiSelectField
+
+availability = (
+    ('1', 'Sunday'),
+    ('2', 'Monday'),
+    ('3', 'Tuesday'),
+    ('4', 'Wednesday'),
+    ('5', 'Thursday'),
+    ('6', 'Friday'),
+    ('7', 'Saturday'),
+)
+
+satisfaction = (
+    ('1', 'Bad'),
+    ('2', 'Neutral'),
+    ('3', 'Good'),
+)
+
 
 # Create your models here.
+class Company(models.Model):
+    social_name = models.CharField(max_length=80)
+    name = models.CharField(max_length=80)
+    cnpj = models.CharField(max_length=14)
+    found_date = models.DateField()
+
+    def __str__(self):
+        return (self.name)
