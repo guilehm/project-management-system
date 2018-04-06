c08509ded9ce765dbafb8df6c086d373c5e207d1
guile.hm <guile.hm@hotmail.com>
2018-04-05 22:03:40 -0300

    html

--- register/views.py
+++ register/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.shortcuts import redirect
 from projects.models import Task
