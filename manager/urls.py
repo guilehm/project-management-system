b741fd150a609abfec897526121e9967e0842d3a
guile.hm <guile.hm@hotmail.com>
2018-04-09 17:38:17 -0300

    Gitgnore

--- manager/urls.py
+++ manager/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
